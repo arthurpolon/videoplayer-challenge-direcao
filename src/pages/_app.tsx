9f1aa0eb8f5989cca76a18dbc01c84348e550695
ARTHUR POLON LEMOS <arthur.lemos@afya.com.br>
2023-01-06 14:45:26 -0300

    📦 NEW: configure Roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import 'styles/global.css';
 import type { AppProps } from 'next/app';
+import { Roboto as getRobotoConfigs } from '@next/font/google';
+
+const roboto = getRobotoConfigs({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={roboto.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
